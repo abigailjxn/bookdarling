b434d78b42fc5162d9230e6f448a0f2a81b1decf
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-22 13:34:28 -0600

    updated API routes, connected to booksController

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,2 +1,15 @@
 const router = require("express").Router;
-const booksController = 
+const booksController = ("../../controllers/booksController");
+
+// "/api/books" route
+router.route("/")
+    .get(booksController.findAll)
+    .post(booksController.create);
+
+// "/api/books/:id" routes
+router.route("/:id")
+    .get(booksController.findbyId)
+    .put(booksController.update)
+    .delete(booksController.delete);
+
+module.exports = router;
