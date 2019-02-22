ec1c5482ea2e71c7e95221bfd1fdcb6eae128652
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-22 14:58:13 -0600

    added Router Path for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Hero from "./components/Hero";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Hero />
-       
-      </div>
-    )
+      <Router>
+        <div>
+          <Switch>
+              <Route exact path="/" component={Search} />
+          </Switch>
+         
+        </div>
+      </Router>
+    );
   }
 }
-
 
 export default App;
