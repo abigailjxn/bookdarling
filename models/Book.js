ee9187cb20031bc30a40e49ef668bd6b0afd326c
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-22 01:54:25 -0600

    added axios, morgan, added Models folder with index.js and Book.js

---
+++ models/Book.js
@@ -0,0 +1,24 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Array,
+        required: true
+    },
+    description: String,
+    image: {
+        type: String,
+        required: true
+    },
+    link: String,
+    
+});
+
+const Book = mongoose.model("Book", BookSchema)
+
+module.exports = Book;
