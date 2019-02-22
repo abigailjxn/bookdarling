f85447cdaa96b5e5522c2c179d82879a315134e3
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-22 13:25:16 -0600

    added backend api routes for books, added controller folder, edited package.json name and description

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const router = require("express").Router;
+const apiRoutes = require("./api");
+
+// API ROUTES
+router.use("/api", apiRoutes);
+
+// IF no API Routes, send React App 
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -2,27 +2,31 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// MONGO DATABASE
 const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bookdarling";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+const routes = require("./routes");
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Routes for API and Client
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// MONGO DATABASE
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bookdarling";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
