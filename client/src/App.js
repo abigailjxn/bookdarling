7a47218cf878779c7bc494cdc64a2f4f37bd3bb2
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-21 21:55:36 -0600

    added UI Kit Node package, added Hero img

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Hero from "./components/Hero";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to my house</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Hero />
+
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.css";
+
+function Hero () {
+    return (
+        <div>
+            <img id="hero-img" src="/images/library.jpg" />
+        </div>
+    )
+}
+
+export default Hero;
