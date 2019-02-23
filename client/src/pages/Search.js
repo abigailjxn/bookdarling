9f1dbc5801e58800c94a811a41ff6034a1d55a7e
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-22 21:13:00 -0600

    added SearchResults, SavedResults, and BookTile components

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
 import Hero from "../components/Hero";
+import SearchReults from "../components/SearchResults";
 
 class Search extends Component {
-    
+
 
     render() {
         return(
             <Hero />
+
+            // SearchResults Coponent
         )
     }
 }
