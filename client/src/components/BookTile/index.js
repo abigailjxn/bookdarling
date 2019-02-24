95daad2d26f13af78449e449e4f145ab7bfee527
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-24 13:49:06 -0600

    added axios to client, added util with API, added google books api key

---
+++ client/src/components/BookTile/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import styles from "./syle.css";
+
+class BookTile extends Component () {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default BookTile;
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
 import styles from "./style.css";
 
+const apiKey = process.env.API_Key;
+let queryString = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query}?key=${apiKey}`;
+
 class SearchBar extends Component {
     state = {
         query: ""
     };
+
+    
 
     handleInputChange = event => {
         const {name, value} = event.target;
