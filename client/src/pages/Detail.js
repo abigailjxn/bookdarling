c7a92643b340954137fe3b4135e0f50636d7ecde
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-03-01 01:12:54 -0600

    styled detail page

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,28 +3,24 @@ import Navigation from "../components/Navigation";
 import API from "../utils/API";
 import "./style.css";
 
-
 const detailHeroStyles = {
-    width: "100vw",
-    height: "100vh",
-    backgroundImage: "url('/images/detail.jpg')"
-}
+  width: "100vw",
+  height: "100vh",
+  backgroundImage: "url('/images/detail.jpg')"
+};
 
-const contentStyles = {
-   
-   
-}
+const contentStyles = {};
 
 class Detail extends Component {
-    state = {
-        book: {}
-    };
+  state = {
+    book: {}
+  };
 
-    componentDidMount() {
-        API.getBook(this.props.match.params.id)
-        .then(res => this.setState({book: res.data}))
-        .catch(err => console.log(err));
-    }
+  componentDidMount() {
+    API.getBook(this.props.match.params.id)
+      .then(res => this.setState({ book: res.data }))
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
@@ -32,10 +28,14 @@ class Detail extends Component {
         <Navigation />
         <body className="content-container" style={contentStyles}>
           <h1 id="saved-header">DETAILS</h1>
+          <div id="book-data">
             <h2 id="detail-book-title">{this.state.book.title}</h2>
             <h3 id="detail-book-author">{this.state.book.author}</h3>
             <p>{this.state.book.description}</p>
-            <span><i class="fas fa-star"></i> | delete</span>
+            <span>
+              <i className="fas fa-star" /> | <span id="detail-delete">delete</span>
+            </span>
+          </div>
         </body>
       </div>
     );
